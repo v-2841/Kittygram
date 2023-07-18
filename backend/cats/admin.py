bb6f921ea543665bec1f9ea869c2d7c2169d41da
Vitaliy Pavlov <v-2841@yandex.ru>
2023-07-18 17:15:56 +0400

    v1

--- backend/cats/admin.py
+++ backend/cats/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 from .models import Achievement, AchievementCat, Cat
 
-
-admin.site.register(Cat)
 admin.site.register(Achievement)
 admin.site.register(AchievementCat)
+admin.site.register(Cat)
